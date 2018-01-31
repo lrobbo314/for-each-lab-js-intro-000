51d580aab4f5920066078342db512fdbdeddfe4b
Robert Lego <robertlego@gmail.com>
2018-01-31 03:34:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function iterativeLog(array) {
 function callback(element, index) {
   console.log(`${index}: ${element}`);
 }
+
+function iterate(callback) {
+  let v = ['Alpha', 'Bravo', 'Charlie'];
+
+  v.foreach(callback);
+  return v;
+}
